0be463986e19c87a8790a68d142bcdc9531a6489
starrs <starrsexplorestheweb@gmail.com>
2026-06-24 00:12:37 +0800

    add back to top button

--- site/tinyspace/miiverse/miiverse-lightbox.js
+++ site/tinyspace/miiverse/miiverse-lightbox.js
@@ -183,3 +183,9 @@ miscCheck.addEventListener("click", (event) => {
     }
   }
 });
+
+let backToTopBtn = document.querySelector("#backToTopBtn");
+
+backToTopBtn.addEventListener("click", (event) => {
+  window.scrollTo(0, 0);
+});

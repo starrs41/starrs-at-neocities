20909443bec3a5c264f3a397270f8b26c96f83e9
starrs <starrsexplorestheweb@gmail.com>
2026-06-24 22:04:16 +0800

    store animation preference in localStorage

--- site/tinyspace/miiverse/scripts/miiverse-anim-toggle.js
+++ site/tinyspace/miiverse/scripts/miiverse-anim-toggle.js
@@ -5,17 +5,50 @@ let animatedElements = [];
 animatedElements.push(document.querySelector("body"));
 animatedElements.push(document.querySelector(".o-navbar"));
 
-animToggle.addEventListener("click", (event) => {
-  if (animToggle.checked) {
-    console.log("play anim");
-    for (const element in animatedElements) {
-      animatedElements[element].classList.remove("u-pause-animation");
-    }
+let animationPref = getAnimationPref();
+
+if (animationPref) {
+  if (animationPref === "true") {
+    playAnimations();
+    setAnimationToggle(true);
   }
   else {
-    console.log("pause anim checked");
-    for (const element in animatedElements) {
-      animatedElements[element].classList.add("u-pause-animation");
-    }
+    pauseAnimations();
+    setAnimationToggle(false);
+  }
+}
+
+animToggle.addEventListener("click", (event) => {
+  if (animToggle.checked) {
+    playAnimations();
+    setAnimationPref("true");
+  }
+  else {
+    pauseAnimations();
+    setAnimationPref("false");
   }
 });
+
+function playAnimations() {
+  for (const element in animatedElements) {
+    animatedElements[element].classList.remove("u-pause-animation");
+  }
+}
+
+function pauseAnimations() {
+  for (const element in animatedElements) {
+    animatedElements[element].classList.add("u-pause-animation");
+  }
+}
+
+function getAnimationPref() {
+  return localStorage.getItem("hasAnimations");
+}
+
+function setAnimationPref(value) {
+  localStorage.setItem("hasAnimations", value);
+}
+
+function setAnimationToggle(value) {
+  animToggle.checked = value;
+}

92f328c59303395f2da56306f51c317568b56099
starrs <starrsexplorestheweb@gmail.com>
2026-07-02 00:59:49 +0800

    create SSG for the individual mii pages

---
+++ ssgs/miiverse/scripts/miiverse_individual.py
@@ -0,0 +1,139 @@
+import json
+from pathlib import Path
+import miiverse_individual_templates as templates
+
+input_folder = "../data"
+output_folder = "../../../site/tinyspace/miiverse"
+
+mii_list = {}
+personality_list = {}
+quirk_list = {}
+gallery_list = {}
+
+with open(f"{input_folder}/miis.json") as f:
+  mii_list = json.load(f)
+
+with open(f"{input_folder}/personality.json") as f:
+  personality_list = json.load(f)
+
+with open(f"{input_folder}/quirk.json") as f:
+  quirk_list = json.load(f)
+
+with open(f"{input_folder}/gallery.json") as f:
+  gallery_list = json.load(f)
+
+
+def populate_personality(mii):
+  slug = mii["personality"]
+  name = personality_list[slug]["name"]
+  description = personality_list[slug]["description"]
+  
+  return templates.personality_template.format(slug=slug, name=name, description=description)
+
+
+def populate_quirks(mii):
+  all_quirk_blocks = []
+  
+  for quirk in mii["quirks"]:
+    name = quirk
+    category = quirk_list[quirk]["category"]
+    quirk_block = templates.quirk_template.format(category=category, alt=category.title(), name=name)
+    all_quirk_blocks.append(quirk_block)
+
+  return "".join(all_quirk_blocks)
+
+
+def populate_details(mii):
+  all_detail_blocks = []
+  
+  for detail in mii["details"]:
+    if not detail == "trivia":
+      label = detail.title()
+      info = mii["details"][detail]
+      detail_block = templates.detail_template.format(label=label, info=info)
+      all_detail_blocks.append(detail_block)
+  
+  return "".join(all_detail_blocks)
+
+
+def populate_trivia(mii):
+  all_trivia_blocks = []
+  
+  for trivia in mii["details"]["trivia"]:
+    trivia_block = templates.trivia_item_template.format(content=trivia)
+    all_trivia_blocks.append(trivia_block)
+  
+  return "".join(all_trivia_blocks)
+
+
+def get_gallery_items(mii):
+    slug = mii["slug"]
+    all_items = []
+
+    for item in gallery_list:
+      if slug in item["miis"]:
+        all_items.append(item)
+    
+    return all_items
+
+
+def populate_thumbnails(items):
+  all_thumbnail_blocks = []
+  
+  for item in items:
+    thumbnail_block = templates.thumbnail_template.format(category=item["category"], filename=item["filename"], alt=item["alt"])
+    all_thumbnail_blocks.append(thumbnail_block)
+  
+  return "".join(all_thumbnail_blocks)
+
+
+def populate_lightbox_info(items):
+  all_info_blocks = []
+  
+  for item in items:
+    info_block = templates.lightbox_info_template.format(info=item["description"])
+    all_info_blocks.append(info_block)
+  
+  return "".join(all_info_blocks)
+
+
+def populate_pictures(items):
+  all_picture_blocks = []
+  
+  for item in items:
+    picture_block = templates.picture_template.format(filename=item["filename"], alt=item["alt"])
+    all_picture_blocks.append(picture_block)
+  
+  return "".join(all_picture_blocks)
+
+
+def create_individual_page(mii):
+  personality = populate_personality(mii)
+  quirks = populate_quirks(mii)
+  details = populate_details(mii)
+  trivia = populate_trivia(mii)
+  gallery_items = get_gallery_items(mii)
+  thumbnails = populate_thumbnails(gallery_items)
+  lightbox_info = populate_lightbox_info(gallery_items)
+  pictures = populate_pictures(gallery_items)
+  
+  final_content = templates.main_template.format(name=mii["name"], description=mii["description"], personality=personality, quirks=quirks, details=details, trivia=trivia, thumbnails=thumbnails, lightbox_info=lightbox_info, pictures=pictures)
+  
+  print(final_content)
+  
+  final_directory = f"{output_folder}/{mii["slug"]}/index.html"
+    
+  final_file = Path(final_directory)
+  final_file.parent.mkdir(exist_ok=True, parents=True)
+  final_file.write_text(final_content)
+  
+  print(f"Page for {mii["name"]} created in {final_directory}")
+
+
+def create_individual_pages():
+  for mii in mii_list:
+    create_individual_page(mii)
+
+
+if __name__ == '__main__':
+    create_individual_pages()
---
+++ ssgs/miiverse/scripts/miiverse_individual_templates.py
@@ -0,0 +1,210 @@
+main_template = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Miiverse - {name}</title>
+  <link rel="stylesheet" href="../misc/styles/miiverse-globals.css">
+  <link rel="stylesheet" href="../misc/styles/miiverse-individual.css">
+</head>
+<body class="u-pause-animation">
+  <div class="o-navbar u-pause-animation">
+    <div class="o-navbar__left">
+      <a class="o-button--icon" href="../">
+        <img src="../../../graphics/misc/miiverse/home.svg" alt="Home Icon"><span>Home</span>
+      </a>
+    </div>
+    <div class="o-navbar__right">
+      <label for="musicToggle" class="o-button o-button--checkbox">
+        <input type="checkbox" id="musicToggle" /><span id="musicToggleText">Music: OFF</span>
+      </label>
+      <label for="animationToggle" class="o-button o-button--checkbox">
+        <input type="checkbox" id="animationToggle" /><span id="animationToggleText">Animations: OFF</span>
+      </label>
+    </div>
+  </div>
+  <div class="o-main--mii">
+    <div class="o-mii-summary">
+      <div class="o-mii-summary__portrait-container">
+        <img class="o-mii-summary__portrait" src="../../../graphics/misc/miiverse/pablo/pablo-idle.gif" alt="Pablo idling" />
+      </div>
+      <div class="o-mii-summary__bio">
+        <h1 class="o-mii-summary__name">
+          {name}
+        </h1>
+        <div class="o-mii-summary__character">
+          {description}
+        </div>
+          {personality}
+      </div>
+    </div>
+    <div class="o-mii-quirk-list">
+      {quirks}
+    </div>
+    <div class="o-mii-details">
+      <h2 class="o-mii-heading">
+        Details
+      </h2>
+      <div class="o-mii-details__list">
+        {details}
+        <div class="o-mii-details__item o-mii-details__item--trivia">
+          <p class="o-mii-details__label">
+            Trivia
+          </p>
+          <ul class="o-mii-details__info o-mii-details__info--trivia">
+            {trivia}
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="o-mii-gallery">
+      <h2 class="o-mii-heading">
+        Gallery
+      </h2>
+      <div class="o-game-list">
+        <label for="tlld" class="o-button o-button--checkbox">
+          <input type="checkbox" id="tlld" checked /> Tomodachi Life: Living the Dream (Switch)
+        </label>
+        <label for="tl" class="o-button o-button--checkbox">
+          <input type="checkbox" id="tl" checked /> Tomodachi Life (3DS)
+        </label>
+        <label for="tc" class="o-button o-button--checkbox">
+          <input type="checkbox" id="tc" checked /> Tomodachi Collection (DS)
+        </label>
+        <label for="miitopiaswitch" class="o-button o-button--checkbox">
+          <input type="checkbox" id="miitopiaswitch" checked /> Miitopia (Switch)
+        </label>
+        <label for="miitopia3ds" class="o-button o-button--checkbox">
+          <input type="checkbox" id="miitopia3ds" checked /> Miitopia (3DS)
+        </label>
+        <label for="misc" class="o-button o-button--checkbox">
+          <input type="checkbox" id="misc" checked /> Miscellaneous
+        </label>
+      </div>
+      <div class="o-thumbnail-list">
+        {thumbnails}
+      </div>
+    </div>
+    <button class="o-top-btn" id="backToTopBtn">
+      Back to Top
+    </button>
+  </div>
+  <div class="o-lightbox u-hidden">
+    <div class="o-lightbox__other">
+      <button class="o-lightbox-info" id="lightboxInfo">
+        <img src="../../../graphics/misc/miiverse/info.svg" alt="Information about the image" />
+        <div class="o-lightbox-info-box u-hidden">
+          {lightbox_info}
+        </div>
+      </button>
+      <button class="o-lightbox-close" id="lightboxClose">
+        <img src="../../../graphics/misc/miiverse/close.svg" alt="Close the lightbox" />
+      </button>
+      <!-- this contains all info -->
+    </div>
+    <div class="o-lightbox__spacing">
+      <div class="o-lightbox__content">
+        <button class="o-lightbox-nav o-lightbox-nav--prev" id="lightboxPrev">
+          <img src="../../../graphics/misc/miiverse/left.svg" alt="Previous image" />
+        </button>
+        <!-- all gallery pictures, hidden by default except for current one -->
+        <div class="o-picture-container">
+          {pictures}
+        </div>
+        <button class="o-lightbox-nav o-lightbox-nav--next" id="lightboxNext">
+          <img src="../../../graphics/misc/miiverse/right.svg" alt="Next image" />
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="o-warning-popup u-hidden">
+    <div class="o-warning-popup__content u-hidden">
+      <div class="o-warning-popup__text">
+        A moment of your time, dear site visitor?
+      </div>
+      <div class="o-warning-popup__buttons">
+        <button class="o-button o-button--popup" id="nextButton">Okay</button>
+      </div>
+    </div>
+    <div class="o-warning-popup__content u-hidden">
+      <div class="o-warning-popup__text">
+        <p>
+          This site contains music and animations.
+        </p>
+        <p>
+          Do you want to enable them?
+        </p>
+      </div>
+      <div class="o-warning-popup__buttons">
+        <button class="o-button o-button--popup" id="yesButton">Sure!</button>
+        <button class="o-button o-button--popup" id="noButton">Nah...</button>
+      </div>
+    </div>
+  </div>
+  <audio loop id="js-bgm">
+    <source src="https://file.garden/akJr1M5SUwY64beP/TLLD%20-%20Mii%20Maker.mp3" type="audio/mpeg">
+    <span>Your browser does not support the audio element.</span>
+  </audio>
+  <script src="../misc/scripts/miiverse-lightbox.js"></script>
+  <script src="../misc/scripts/miiverse-gallery-filter.js"></script>
+  <script src="../misc/scripts/miiverse-top-button.js"></script>
+  <script src="../misc/scripts/miiverse-anim-toggle.js"></script>
+  <script src="../misc/scripts/miiverse-music-toggle.js"></script>
+  <script src="../misc/scripts/miiverse-warning-popup.js"></script>
+</body>
+</html>
+"""
+
+personality_template = """
+        <div class="o-mii-personality o-mii-personality--{slug}">
+          <p class="o-mii-personality__name">
+            {name}
+          </p>
+          <p class="o-mii-personality__desc">
+            {description}
+          </p>
+        </div>
+"""
+
+quirk_template = """
+      <div class="o-mii-quirk">
+        <img src="../../../graphics/misc/miiverse/icon-{category}.png" alt="{alt} Icon"><span>{name}</span>
+      </div>
+"""
+
+# 0 - label
+# 1 - info
+
+detail_template = """ 
+        <div class="o-mii-details__item">
+          <p class="o-mii-details__label">
+            {label}
+          </p>
+          <p class="o-mii-details__info">
+            {info}
+          </p>
+        </div>
+"""
+
+trivia_item_template = """
+            <li>
+              {content}
+            </li>
+"""
+
+thumbnail_template = """
+        <div class="o-thumbnail-container">
+          <img data-game="{category}" class="o-thumbnail" src="../../../graphics/misc/miiverse/gallery/{filename}" alt="{alt}" />
+        </div>
+"""
+
+lightbox_info_template = """
+          <p class="o-lightbox-info-box__content u-hidden">
+            {info}
+          </p>
+"""
+
+picture_template = """
+          <img class="o-picture u-hidden" src="../../../graphics/misc/miiverse/gallery/{filename}" alt="{alt}" />
+"""
